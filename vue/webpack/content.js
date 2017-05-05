b6492429cad33bda6a8f388139d93c181c5ade85
wangding <1024564775@qq.com>
2017-05-05 18:47:24 +0800

    add vue module

---
+++ vue/webpack/content.js
@@ -0,0 +1,4 @@
+/**
+ * Created by wang.ding on 2017/5/5.
+ */
+module.exports = "It works from content.js";
---
+++ vue/webpack/entry.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wang.ding on 2017/5/5.
+ */
+// require("!style!css!./style.css");
+    require("./style.css");
+document.write(require("./content.js"));
